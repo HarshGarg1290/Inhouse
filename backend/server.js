d5f6393693180e6eaf4da70c5929f54b03439af5
Harsh Garg <hg8728@srmist.edu.in>
2025-05-12 22:11:24 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -42,5 +42,5 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/rides", rideRoutes);
 app.use("/api/users", userRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+export default app;
